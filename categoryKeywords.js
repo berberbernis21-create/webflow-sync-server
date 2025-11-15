9817399e2e3adbb9275467b0052f5f9b1b5c7434
Bernis Berber <berberbernis21@gmail.com>
2025-11-15 10:48:29 -0500

    Add files via upload

---
+++ categoryKeywords.js
@@ -0,0 +1,63 @@
+export const CATEGORY_KEYWORDS = {
+  Handbags: [
+    "handbag", "shoulder bag", "top handle", "hobo", "satchel",
+    "bowler", "structured", "shopper", "shopping bag",
+    "doctor bag", "frame bag", "barrel bag", "bucket handbag",
+    "north south bag", "east west bag", "musubi", "jumbo flap",
+    "single flap", "day bag", "mini shoulder bag", "boston bag",
+    "bowling bag", "moon bag", "kiss-lock", "kiss lock",
+    "frame handbag", "two-way handbag", "fold-over", "heroine", "faye"
+  ],
+
+  Totes: [
+    "tote", "cabas", "cabata", "book tote", "shopping tote",
+    "east west tote", "north south tote", "carryall", "market tote",
+    "work tote", "perforated tote", "open tote", "soft tote",
+    "padlock tote", "duplex tote", "rive gauche", "mezzo", "mezzo tote"
+  ],
+
+  Crossbody: [
+    "crossbody", "cross body", "camera bag", "messenger",
+    "saddle bag", "wallet on chain", "woc", "chain bag",
+    "sling bag", "reporter bag", "trio bag", "multi pochette",
+    "compact crossbody", "mini crossbody", "flap crossbody",
+    "half moon", "demi lune", "phone bag", "phone holder with chain"
+  ],
+
+  "Small Bags": [
+    "mini bag", "micro bag", "nano bag", "small bag", "pochette",
+    "clutch", "wristlet", "evening bag", "minaudiere", "pouch",
+    "card case with chain", "key pouch with chain", "mini vanity",
+    "mini box bag", "micro box", "wallet on strap",
+    "baguette bag", "compact shoulder bag"
+  ],
+
+  Backpacks: [
+    "backpack", "mini backpack", "daypack", "rucksack",
+    "drawstring backpack", "convertible backpack",
+    "2-way backpack", "sling/backpack", "school backpack",
+    "city backpack"
+  ],
+
+  Wallets: [
+    "wallet", "zippy", "trifold wallet", "bifold wallet", "cardholder",
+    "card holder", "key pouch", "pochette wallet", "compact wallet",
+    "continental wallet", "long wallet", "passport holder",
+    "passport case", "fold wallet", "envelope wallet",
+    "flap wallet", "coin purse", "checkbook cover",
+    "organizer pouch"
+  ],
+
+  Luggage: [
+    "briefcase", "travel case", "weekender", "duffle", "duffel",
+    "keepall", "carry-on", "garment bag", "overnight bag",
+    "pilot case", "business bag", "portfolio bag", "roller bag",
+    "rolling suitcase", "train case", "steamer bag", "travel pouch"
+  ],
+
+  Accessories: [
+    "belt bag", "belt", "bag charm", "keychain", "strap",
+    "dust bag", "cosmetic pouch", "cosmetic case", "makeup bag",
+    "vanity case", "sunglasses case", "eyeglass case", "phone case"
+  ]
+};
